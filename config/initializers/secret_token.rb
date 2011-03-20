1bb945a481d1c571756edb080c3a8b6c84d586d2
a-freenomenus <leaser.geen@gmail.com>
2011-03-20 17:27:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wannabuy::Application.config.secret_token = '992f450e4b1cc47b0e1cea9aa0bfdcb7cd85a4cf652ffb75d15c2ffed739b2e32a439fd957eb129d36582cb736fef69b719b9f1432f0bcb54c3a33bb5d23ed85'
